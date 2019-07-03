29fa7d97a06ae181ac41f0c35ee0d6e8695ebda9
Eli Schleifer <elischleifer@uber.com>
2019-07-03 15:36:38 -0700

    test

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
+#!/usr/bin/env bashio
+
 echo Starting Homeassistant Doorbell
 
-#!/bin/bash
-
 pip3 install mutagen
+
+
+USERNAME=$(bashio::config 'username')
+
+bashio::log.info "${USERNAME}"
+
+bashio::config.info "${USERNAME}"
+
+bashio::log.info "$(bashio::hardware)"
 
 python3 -u doorbell-server.py
