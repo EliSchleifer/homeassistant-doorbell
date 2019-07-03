3af1cb15e5a974fc40ee307a8f123bf6ecf87452
Eli Schleifer <elischleifer@uber.com>
2019-07-03 16:18:19 -0700

    stuf

--- run.sh
+++ run.sh
@@ -9,12 +9,6 @@ USERNAME=$(bashio::config "username")
 
 bashio::log.info "${USERNAME}"
 
-
-EXISTS=$(bashio::config.exists "HomeAssistantIP")
-
-bashio::log.info "${EXISTS}"
-
-
 IP_ADDRESS=$(bashio::config "HomeAssistantIP")
 
 bashio::log.info "${IP_ADDRESS}"
