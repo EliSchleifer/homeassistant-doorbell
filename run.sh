afb6ffde02b3c34d1f98d62c575803f93d5a1486
Eli Schleifer <elischleifer@outlook.com>
2019-07-05 09:29:41 -0700

    Run server

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bashio
 
+bashio::log.yellow "Booting up..."
+
 IP_ADDRESS=$(bashio::config "ip_address")
-bashio::log.info "${IP_ADDRESS}"
+bashio::log.info "Configured to  use ${IP_ADDRESS} as root IP"
 
 pip install mutagen
 
-python -u doorbell-server.py
+python -u doorbell-server.py 
